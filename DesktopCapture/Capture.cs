891d2febfbdec2a4dd506b928074876b1778f6d0
Shubham Singh <shubham.nfynt@gmail.com>
2020-11-15 19:41:43 +0100

    In place texture update for Unity and updated LoadImage API

--- DesktopCapture/Capture.cs
+++ DesktopCapture/Capture.cs
@@ -8,70 +8,57 @@ namespace Shubham.DesktopCapLib
 {
     public class Capture
     {
-        public struct TexInfo
-        {
-            public UnityEngine.Texture2D unityTex2D;
-            public byte[] bitStream;
-        }
 
         public UnityEngine.Texture2D tex;
         public Rectangle screenSize;
-        public TexInfo texInfo;
 
+		private Bitmap target;
+		private MemoryStream target_ms;
+		private int width, height;
+		private int startX, startY;
 
-        public Capture()
+		/// <summary>
+		/// Get active screen(s) information
+		/// </summary>
+		/// <returns>string info for screen in new line</returns>
+		public static string GetScreenInfo()
+		{
+			string info = "";
+			int ind = 0;
+			foreach (Screen sc in Screen.AllScreens)
+			{
+				info += ind + ":" + sc.DeviceName + " size:" + sc.WorkingArea.ToString() + "\n";
+				ind++;
+			}
+
+			return info;
+		}
+
+		public Capture(int x=0, int y=0, int width=0, int height=0)
         {
-            tex = new UnityEngine.Texture2D(20, 30, UnityEngine.TextureFormat.DXT1, false);
-            screenSize = Screen.PrimaryScreen.Bounds;
-        }
+			screenSize = Screen.PrimaryScreen.Bounds;
+			UpdateCaptureScreen(x, y, width, height);
+		}
+        
+		/// <summary>
+		/// Set new Capture screen size
+		/// </summary>
+		/// <param name="x">source start x</param>
+		/// <param name="y">source start y</param>
+		/// <param name="width">source width</param>
+		/// <param name="height">source height</param>
+		public void UpdateCaptureScreen(int x, int y, int width, int height)
+		{
+			tex = new UnityEngine.Texture2D(20, 30, UnityEngine.TextureFormat.DXT1, false);
 
-        /// <summary>
-        /// Get the current screen information.
-        /// </summary>
-        /// <returns>struct <i>TexInfo</i> which contains <i>Texture2D</i> and <i>byte</i> array</returns>
-        public TexInfo GetCurrScreenInfo()
-        {
-            Bitmap target = new Bitmap(screenSize.Width, screenSize.Height);
-            using (Graphics g = Graphics.FromImage(target))
-            {
-                g.CopyFromScreen(0, 0, 0, 0, new Size(screenSize.Width, screenSize.Height));
-            }
+			startX = x;
+			startY = y;
+			this.width = (width != 0) ? width : screenSize.Width;
+			this.height = (height != 0) ? height : screenSize.Height;
 
-            MemoryStream ms = new MemoryStream();
-            target.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
-            ms.Seek(0, SeekOrigin.Begin);
-
-            this.texInfo.bitStream = ms.ToArray();
-            this.texInfo.unityTex2D.LoadImage(this.texInfo.bitStream);
-
-            return this.texInfo;
-        }
-
-        /// <summary>
-        /// Get the current screen information for block of the screen
-        /// </summary>
-        /// <param name="x">start x</param>
-        /// <param name="y">xtart y</param>
-        /// <param name="width">width of the block</param>
-        /// <param name="height">height of the block</param>
-        ///  <returns>struct <i>TexInfo</i> which contains <i>Texture2D</i> and <i>byte</i> array</returns>
-        public TexInfo GetCurrScreenInfo(int x, int y, int width, int height)
-        {
-            Bitmap target = new Bitmap(screenSize.Width, screenSize.Height);
-            using (Graphics g = Graphics.FromImage(target))
-            {
-                g.CopyFromScreen(x, y, 0, 0, new Size(width, height));
-            }
-
-            MemoryStream ms = new MemoryStream();
-            target.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
-            ms.Seek(0, SeekOrigin.Begin);
-
-            this.texInfo.bitStream = ms.ToArray();
-            this.texInfo.unityTex2D.LoadImage(this.texInfo.bitStream);
-
-            return this.texInfo;
-        }
+			target = new Bitmap(this.width - x, this.height - y);
+			target_ms = new MemoryStream();
+		}
 
         /// <summary>
         /// Get the current screen in Unity Texture2D format.
@@ -79,22 +66,35 @@ namespace Shubham.DesktopCapLib
         /// <returns>struct <i>TexInfo</i> which contains <i>Texture2D</i> and <i>byte</i> array</returns>
         public UnityEngine.Texture2D GetScreen()
         {
-           // UnityEngine.Texture2D tex = new UnityEngine.Texture2D(20,30,UnityEngine.TextureFormat.DXT1,false);
-            //Rectangle screenSize = Screen.PrimaryScreen.Bounds;
-            Bitmap target = new Bitmap(screenSize.Width, screenSize.Height);
             using (Graphics g = Graphics.FromImage(target))
             {
-                g.CopyFromScreen(0, 0, 0, 0,new Size(screenSize.Width, screenSize.Height));
+                g.CopyFromScreen(startX, startY, 0, 0,new Size(width,height));
             }
 
-            MemoryStream ms = new MemoryStream();
-            target.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
-            ms.Seek(0, SeekOrigin.Begin);
+            target.Save(target_ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+            target_ms.Seek(0, SeekOrigin.Begin);
 
-            tex.LoadImage(ms.ToArray());
+			UnityEngine.ImageConversion.LoadImage(tex, target_ms.ToArray());
 
             return tex;
         }
+
+		/// <summary>
+		/// Update the Unity texture 2d in place
+		/// </summary>
+		/// <param name="texture">Texture2D object to be updated</param>
+		public void UpdateUnityTexture(ref UnityEngine.Texture2D texture)
+		{
+			using (Graphics g = Graphics.FromImage(target))
+			{
+				g.CopyFromScreen(startX,startY, 0, 0, new Size(width,height));
+			}
+
+			target.Save(target_ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+			target_ms.Seek(0, SeekOrigin.Begin);
+
+			UnityEngine.ImageConversion.LoadImage(texture, target_ms.ToArray());
+		}
 
         /// <summary>
         /// Saves the current frame at specified location.
@@ -113,33 +113,8 @@ namespace Shubham.DesktopCapLib
             ms.Seek(0, SeekOrigin.Begin);
             File.WriteAllBytes(loc, ms.ToArray());
         }
-
-        /// <summary>
-        /// Get a block of the screen
-        /// </summary>
-        /// <param name="x">start x</param>
-        /// <param name="y">xtart y</param>
-        /// <param name="width">width of the block</param>
-        /// <param name="height">height of the block</param>
-        /// <returns></returns>
-        public UnityEngine.Texture2D GetScreen(int x, int y, int width, int height)
-        {
-            // UnityEngine.Texture2D tex = new UnityEngine.Texture2D(20,30,UnityEngine.TextureFormat.DXT1,false);
-
-            //Rectangle screenSize = Screen.PrimaryScreen.Bounds;
-            Bitmap target = new Bitmap(width, height);
-            using (Graphics g = Graphics.FromImage(target))
-            {
-                g.CopyFromScreen(x, y, 0, 0, new Size(width, height));
-            }
-
-            MemoryStream ms = new MemoryStream();
-            target.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
-            ms.Seek(0, SeekOrigin.Begin);
-
-            tex.LoadImage(ms.ToArray());
-
-            return tex;
-        }
+		
     }
 }
+
+
